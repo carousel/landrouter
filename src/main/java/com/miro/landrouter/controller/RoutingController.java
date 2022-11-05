3ce1856588b95bfb1962aed491ae155a0379ce21
Miroslav Trninic <miroslav.trninic@gmail.com>
2022-11-05 20:11:27 +0100

    add support route to fetch all countries

--- src/main/java/com/miro/landrouter/controller/RoutingController.java
+++ src/main/java/com/miro/landrouter/controller/RoutingController.java
@@ -1,6 +1,8 @@
 package com.miro.landrouter.controller;
 
+import com.miro.landrouter.data.Country;
 import com.miro.landrouter.model.Route;
+import com.miro.landrouter.service.CountryService;
 import com.miro.landrouter.service.RoutingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -18,9 +21,17 @@ import java.io.IOException;
 public class RoutingController {
 
   private final RoutingService routingService;
+
+  private final CountryService countryService;
+
   @GetMapping("/routing/{origin}/{destination}")
-  public ResponseEntity<Route> originDestination(@PathVariable String origin, @PathVariable String destination) throws IOException {
+  public ResponseEntity<Route> getRoute(@PathVariable String origin, @PathVariable String destination) throws IOException {
     var route = routingService.route(origin, destination);
     return ResponseEntity.ok(route);
+  }
+  @GetMapping("/countries")
+  public ResponseEntity<List<Country>> getCountries() throws IOException {
+    var countries = countryService.countries();
+    return ResponseEntity.ok(countries);
   }
 }
